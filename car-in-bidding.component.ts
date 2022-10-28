180048fc9df67164534e22abec7f3b6681a75409
Sanup <sanup.cse@gmail.com>
2022-10-28 10:27:16 +0530

    upload

---
+++ car-in-bidding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-in-bidding',
+  templateUrl: './car-in-bidding.component.html',
+  styleUrls: ['./car-in-bidding.component.css']
+})
+export class CarInBiddingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
